9193fa83f0d5e9661c5cda9d1ed822fe4965458e
Kushal Gaiwala <kushalgaywala@gmail.com>
2021-08-28 23:45:17 +0530

    Added session and study time and break time

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
-const timer = document.getElementById("timer");
+const timer = document.getElementById("time");
 const todate = document.getElementById("date");
+const button = document.getElementById("button");
+const studyTime = document.getElementById("study-time");
+const breakTime = document.getElementById("break-time");
+const session = document.getElementById("sessions");
+const context = document.getElementById("context");
+const sTimer = document.getElementById("timer");
 
-setInterval(function () {
+// watch START
+let watchInterval = setInterval(function () {
   let today = new Date();
   let month = today.getMonth() + 1;
   let day = today.getDay();
@@ -12,7 +19,93 @@ setInterval(function () {
 
   timer.innerHTML = pad(hour) + ":" + pad(minute) + ":" + pad(seconds);
   todate.innerHTML = getDay(day) + ", " + getMonth(month) + " " + pad(date);
-}, 1000);
+});
+// watch END
+
+let min = 0;
+let sec = 0;
+let sess = session.value;
+let swtch = 0;
+
+let timerInterval;
+button.addEventListener("click", function () {
+  if (button.value === "Start") {
+    button.value = "Stop";
+    timerInterval = setInterval(funInterval, 1000);
+  } else if (button.value === "Stop") {
+    button.value = "Start";
+    clearInterval(timerInterval);
+  }
+});
+
+function funInterval() {
+  if (sec < 59 && min < studyTime.value && min < breakTime.value) {
+    ++sec;
+  } else if (min < 59 && min < studyTime.value && min < breakTime.value) {
+    sec = 0;
+    ++min;
+  } else {
+    if (context.innerHTML === "Study Timer") {
+      context.innerHTML = "Break Timer";
+      swtch = 1;
+    } else if (context.innerHTML === "Break Timer") {
+      context.innerHTML = "Study Timer";
+      swtch = 0;
+    }
+    min = 0;
+    sec = 0;
+    --sess;
+    if (sess > 1) {
+      clearInterval(timerInterval);
+    }
+  }
+  sTimer.innerHTML = newTime(pad(min), pad(sec));
+  console.log(newTime(min, sec));
+}
+
+// countdown START
+// let interval;
+// let sTime = studyTime.value * 60;
+// let bTime = breakTime.value * 60;
+// let sess = session.value;
+
+// button.addEventListener("click", function (event) {
+//   if (event.target.value === "Start") {
+//     event.target.value = "Stop";
+//     interval = setInterval(funInterval, 1000);
+//   } else {
+//     clearInterval(interval);
+//     event.target.value = "Start";
+//   }
+// });
+
+// function funInterval() {
+//   newTime = pad(bTime) + ":" + pad(sTime);
+//   if (sNew < sTime && sess > 0) {
+//     console.log("study: " + sTime);
+//     context.innerHTML = "Study Timer";
+//     sTimer.innerHTML = pad(sTime);
+//     --sTime;
+//   } else if (bNew < bTime && sess > 1) {
+//     console.log("break: " + bTime);
+//     context.innerHTML = "Break Timer";
+//     sTimer.innerHTML = pad(bTime);
+//     --bTime;
+//   } else if (sess > 1) {
+//     sTime = studyTime.value * 60;
+//     bTime = breakTime.value * 60;
+//     context.innerHTML += " " + sess;
+//     console.log("session: " + sess);
+//     --sess;
+//   } else {
+//     sTimer.innerHTML = newTime(0, 0);
+//   }
+// }
+// countdown END
+
+function newTime(minute, second) {
+  return minute + ":" + second;
+}
 
 function pad(digit) {
   return digit < 10 ? "0" + digit : digit;
